e9292681db63522c017d291f95e44f545d2ef743
Or Noyman <or.noyman@gmail.com>
2021-02-06 09:21:55 +0200

    update tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.4.10"
+    kotlin("multiplatform") version "1.4.30"
+    id("maven-publish")
 }
 
 repositories {
@@ -16,16 +17,40 @@ kotlin {
             }
         }
     }
-    js {
-        nodejs()
-        binaries.executable()
+    js(BOTH) {
+        nodejs {
+            testTask {
+                useMocha {
+                    timeout = "100000"
+                }
+            }
+        }
     }
 
     sourceSets {
-        val commonMain by getting {
+        commonMain {
             dependencies {
-                implementation("com.ionspin.kotlin:bignum:0.2.0")
+                implementation("com.ionspin.kotlin:bignum:0.2.4")
                 implementation("com.soywiz.korlibs.krypto:krypto:1.12.0")
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
             }
         }
     }
